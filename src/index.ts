bcf139f472c3be79c527825f3e35f10eee7cb892
Zyldzkie <73383753+mmatias1988@users.noreply.github.com>
2026-01-08 04:17:01 +0800

    update: Apollo Server defs and resolvers integration

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
+import 'dotenv/config';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { leadTypeDefs, leadResolvers } from './modules/leads/index.js';
 
+/**
+ * Combined GraphQL schema
+ * Includes health check query merged with lead schema
+ */
 const typeDefs = `#graphql
-  type Query {
+  ${leadTypeDefs}
+
+  extend type Query {
     health: String
   }
 `;
 
+/**
+ * Combined GraphQL resolvers
+ */
 const resolvers = {
   Query: {
     health: () => 'OK',
+    ...leadResolvers.Query,
   },
+  Mutation: {
+    ...leadResolvers.Mutation,
+  },
+  DateTime: leadResolvers.DateTime,
 };
 
 const server = new ApolloServer({
@@ -22,7 +38,7 @@ async function startServer() {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
-  console.log(`Server is running on ${url}`);
+  console.log(`GraphQL Playground available at ${url}`);
 }
 
 startServer().catch((error) => {
